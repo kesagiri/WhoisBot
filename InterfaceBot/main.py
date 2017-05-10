17fffd78250c4d2f0f91d727f11ef4368add978a
Anton <antony.rus@yandex.ru>
2017-05-10 09:08:12 +0300

    add commands in main

--- InterfaceBot/main.py
+++ InterfaceBot/main.py
@@ -18,7 +18,7 @@ def handle_text(message):
     user_markup.row('Перенести домен', 'Владелец домена')
     user_markup.row('Владелец аккаунта', 'Восстановить пароль')
     user_markup.row('покажи кошака')
-    bot.send_message(message.from_user.id, text.start, reply_markup=user_markup)
+    bot.send_message(message.from_user.id, text.start, reply_markup=user_markup, parse_mode="HTML")
 
 
 # Выключить клавиатуру
@@ -34,10 +34,40 @@ def handle_help(message):
     bot.send_message(message.chat.id, text.start, parse_mode="HTML")
 
 
-# Вывод команды /different
+# Вывод команды по различию доменов /different
 @bot.message_handler(commands=['difference'])
 def handle_help(message):
     bot.send_message(message.chat.id, text.difference, parse_mode="HTML")
+
+
+# Вывод команды по различию доменов /different
+@bot.message_handler(commands=['pay'])
+def handle_help(message):
+    bot.send_message(message.chat.id, text.pay, parse_mode="HTML")
+
+
+# Вывод команды по различию доменов /different
+@bot.message_handler(commands=['change_admin'])
+def handle_help(message):
+    bot.send_message(message.chat.id, text.change_adm, parse_mode="HTML")
+
+
+# Вывод команды по различию доменов /different
+@bot.message_handler(commands=['domain_transfer'])
+def handle_help(message):
+    bot.send_message(message.chat.id, text.transfer, parse_mode="HTML")
+
+
+# Вывод команды по различию доменов /different
+@bot.message_handler(commands=['account'])
+def handle_help(message):
+    bot.send_message(message.chat.id, text.own_account1, parse_mode="HTML")
+
+
+# Вывод команды по различию доменов /different
+@bot.message_handler(commands=['return_account'])
+def handle_help(message):
+    bot.send_message(message.chat.id, text.were_password, parse_mode="HTML")
 
 
 # Обработка сообщений и команд
